6f66e96ad69d3dc0b0c7c1d0fe811063c7534fe4
Danish Raza <danishraza0333@gmail.com>
2021-08-12 21:39:19 +0500

    Add JavaScript files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
+
+const App = () => {
+    return (
+        <div className="ui container comments">
+            <ApprovalCard approve="Approve" reject="Reject">
+                <CommentDetail
+                    author="Danish"
+                    timeAgo="Today at 4:45PM"
+                    content="Nice blog post!"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard approve="Approve" reject="Reject">
+                <CommentDetail
+                    author="Alex"
+                    timeAgo="Today at 2:00AM"
+                    content="Interesting content!"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard approve="Approve" reject="Reject">
+                <CommentDetail
+                    author="Sam"
+                    timeAgo="Yesterday at 3:00PM"
+                    content="Great blog!"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard approve="Approve All" reject="Reject All">
+                <h4>Alert!</h4>
+                <div>
+                    Are you sure you want to Approve/Reject all?
+                </div>
+            </ApprovalCard>
+        </div>
+    );
+};
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <App />,
+    document.querySelector("#root")
+)
